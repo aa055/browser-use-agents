c6f411a44a2393f1e50e3a6c688fcab2375b8838
Aamir Ali <64472870+aa055@users.noreply.github.com>
2025-12-24 19:50:58 +0400

    Add files via upload
    Other agents workflow tests

---
+++ follow_up_tasks.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+
+from browser_use import Agent, Browser
+
+
+load_dotenv()
+
+import asyncio
+
+
+async def main():
+	browser = Browser(keep_alive=True)
+
+	await browser.start()
+
+	agent = Agent(task='search for browser-use.', browser_session=browser)
+	await agent.run(max_steps=2)
+	agent.add_new_task('return the title of first result')
+	await agent.run()
+
+	await browser.kill()
+
+asyncio.run(main())
